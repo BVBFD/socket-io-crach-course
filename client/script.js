b952663bbea6eed5b9b78eb8382a4efb7d6dce11
BVBFD <lsevina126@gmail.com>
2022-05-18 08:21:11 +0900

    Code Join Room and able to take part in private rooms

--- client/script.js
+++ client/script.js
@@ -1,38 +1,41 @@
-import { io } from 'socket.io-client'
+import { io } from 'socket.io-client';
 
-const joinRoomButton = document.getElementById('room-button')
-const messageInput = document.getElementById('message-input')
-const roomInput = document.getElementById('room-input')
-const form = document.getElementById('form')
+const joinRoomButton = document.getElementById('room-button');
+const messageInput = document.getElementById('message-input');
+const roomInput = document.getElementById('room-input');
+const form = document.getElementById('form');
 
-const socket = io('http://localhost:3000')
+const socket = io('http://localhost:3000');
 socket.on('connect', () => {
-  displayMessage(`You connected with id: ${socket.id}`)
-})
+  displayMessage(`You connected with id: ${socket.id}`);
+});
 
 socket.on('receive-message', (message) => {
-  displayMessage(message)
-})
+  displayMessage(message);
+});
 
 // socket.emit('custom-event', 10, 'Hi', { a: 'a' })
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const message = messageInput.value
-  const room = roomInput.value
+  e.preventDefault();
+  const message = messageInput.value;
+  const room = roomInput.value;
 
-  if (message === '') return
-  displayMessage(message)
-  socket.emit('send-message', message)
+  if (message === '') return;
+  displayMessage(message);
+  socket.emit('send-message', message, room);
 
-  messageInput.value = ''
-})
+  messageInput.value = '';
+});
 
 joinRoomButton.addEventListener('click', () => {
-  const room = roomInput.value
-})
+  const room = roomInput.value;
+  socket.emit('join-room', room, (message) => {
+    displayMessage(message);
+  });
+});
 
 const displayMessage = (message) => {
-  const div = document.createElement('div')
-  div.textContent = message
-  document.getElementById('message-container').append(div)
-}
+  const div = document.createElement('div');
+  div.textContent = message;
+  document.getElementById('message-container').append(div);
+};
--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
-import { Server } from 'socket.io'
+import { Server } from 'socket.io';
 
 const io = new Server(3000, {
   cors: {
-    origin: ['http://localhost:8080', 'http://localhost:8081'],
+    origin: [
+      'http://localhost:8080',
+      'http://localhost:8081',
+      'http://localhost:8082',
+    ],
   },
-})
+});
 
 io.on('connection', (socket) => {
-  console.log(`A user ${socket.id} connected!`)
+  console.log(`A user ${socket.id} connected!`);
   //   socket.on('custom-event', (number, string, obj) => {
   //     console.log(number, string, obj)
   //   })
 
-  socket.on('send-message', (message) => {
-    socket.broadcast.emit('receive-message', message)
-  })
-})
+  socket.on('send-message', (message, room) => {
+    if (room === '') {
+      socket.broadcast.emit('receive-message', message);
+    } else {
+      socket.to(room).emit('receive-message', message);
+    }
+  });
+
+  socket.on('join-room', (room, cb) => {
+    socket.join(room);
+    cb(`Joined Room With ${room}`);
+  });
+});
